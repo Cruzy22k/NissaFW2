9aaea72886f3f31b3a82c10cbcea1a6e3ec47b73
Cruzy22k <zakisullivan63@gmail.com>
2025-03-01 13:32:17 +1100

    bruh

--- firmware.sh
+++ firmware.sh
@@ -16,9 +16,10 @@ RECOVERY_KEY_DEDEDE_FILE="dedede_recovery_v1.vbpubk"
 RECOVERY_KEY_CORSOLA="https://raw.githubusercontent.com/Cruzy22k/NissaFW2/main/corsola_recovery_v1.vbpubk"
 RECOVERY_KEY_CORSOLA_FILE="corsola_recovery_v1.vbpubk"
 
-echo -e "\e[1;32m<Firmware2>  Copyright (C) 2024  Cruzy22k\e[0m"
-echo -e "\e[1mThis program comes with ABSOLUTELY NO WARRANTY.\e[0m"
-echo -e "This is free software, and you are welcome to redistribute it under certain conditions.\n"
+echo -e "\e[32m<Firmware2>  Copyright (C) 2024  Cruzy22k\e[0m"
+echo -e "\e[32mThis program comes with ABSOLUTELY NO WARRANTY.\e[0m"
+echo -e "\e[32mThis is free software, and you are welcome to redistribute it under certain conditions.\e[0m"
+echo
 
 echo "please select what device you have, dedede (1), nissa (2) or corsola (3)"
 echo    
