ec122120be2bbbec11f08acf6f0bc761523a4444
Cruzy22k <zakisullivan63@gmail.com>
2025-03-01 13:39:10 +1100

    Update firmware.sh
    removed comments (😢 )

--- firmware.sh
+++ firmware.sh
@@ -2,13 +2,13 @@
 
 # This script downloads a recovery key file and applies it to the firmware using futility.  This script is ONLY intended for use on Dedede, Nissa or Corsola. Use at your own risk. I might add more keyrolled devices in the near future
 # This script requires to be ran as chronos, not root. Failure to do so may result in the recovery key not being applied correctly, or the device not being able to access the recovery key file.
-# Detect ChromeOS/ChromiumOS
+
 if grep -q "Chrom" /etc/lsb-release ; then
     DOWNLOADS_DIR="/home/chronos/user/Downloads"
 else
     DOWNLOADS_DIR="$HOME/Downloads"
 fi
-# Variables
+
 RECOVERY_KEY_NISSA="https://raw.githubusercontent.com/Cruzy22k/NissaFW2/main/nissa_recovery_v1.vbpubk"
 RECOVERY_KEY_NISSA_FILE="nissa_recovery_v1.vbpubk"
 RECOVERY_KEY_DEDEDE="https://raw.githubusercontent.com/Cruzy22k/NissaFW2/main/dedede_recovery_v1.vbpubk"
@@ -49,16 +49,15 @@ echo "DEBUG: Selected file name: $RECOVERY_KEY_FILE"
 
 
 
-# Create Downloads directory if it doesn't exist
+
 mkdir -p "$DOWNLOADS_DIR"
 
 
-# Download the recovery key file to the Downloads folder
+
 echo "Downloading the recovery key file..."
 cd "$DOWNLOADS_DIR" || exit 1
 curl -L -o "$RECOVERY_KEY_FILE" "$RECOVERY_KEY_URL"
 
-# Check if download was successful
 if [ ! -f "$RECOVERY_KEY_FILE" ]; then
     echo "Failed to download the recovery key file."
     exit 1
@@ -76,7 +75,7 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
-# Apply the recovery key with futility
+
 echo "Applying the recovery key with futility..."
 futility gbb -s --flash --recoverykey="$DOWNLOADS_DIR/$RECOVERY_KEY_FILE"
 
@@ -98,7 +97,7 @@ echo " "
 echo "Made by cruzy22k" 
 echo ":)"
 echo " A reboot is required for the changes to take effect."
-# Clear the vbpubk files from the Downloads folder
+
 echo "Clearing the vbpubk files from the Downloads folder..."
 rm -f "$DOWNLOADS_DIR"/*.vbpubk
 
@@ -110,7 +109,7 @@ echo "the vbpubk files have been removed from the Downloads folder."
 echo " "
 
 
-# Option to reboot the system
+
 read -p "Do you want to reboot now? (y/n) " -n 1 -r
 echo   
 if [[ $REPLY =~ ^[Yy]$ ]]; then
